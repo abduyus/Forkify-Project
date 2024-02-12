0593e706e3842f7511bb0a1a73f32df420516325
abduyus <35887528+abduyus@users.noreply.github.com>
2024-02-12 10:39:40 +0000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0c17d6ba-feb8-4006-8b07-16e6589b27cf';
+export const MODAL_CLOSE_SEC = 2.5;
